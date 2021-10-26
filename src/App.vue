956ae3781f8b1064aba2b67aa63b04d984d583e4
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2021-10-26 10:12:46 +0700

    chore: config fixes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
 import { rsaGenerateKey, rsaEncrypt } from "./utils/cryptography";
 
 const result = ref(0n);
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import windicss from "vite-plugin-windicss";
@@ -6,12 +7,18 @@ import autoimport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
   plugins: [
     vue(),
     windicss(),
     components(),
     autoimport({
       imports: "vue",
+      include: [/\.vue$/, /\.vue\?vue/],
     }),
   ],
 });
